e949655ad05cf89cb442f14352c77062d859c21b
prabhpreetgill <116679689+prabhpreetgill@users.noreply.github.com>
2023-05-09 22:58:02 -0400

    make it better

--- app.js
+++ app.js
@@ -87,6 +87,11 @@ app.get("/:postName", (req, res) => {
 });
 })
 
-app.listen(4000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 8000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port %d", port);
 });
